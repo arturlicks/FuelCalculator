952d5ec519d5d71b105f2ad36677693ae87ad4ad
arturlicks <arturlicks@gmail.com>
2018-02-01 22:02:09 -0200

    Fixed the main routes, the post arguments and the form. Fixed the List with all fields.

--- Main.py
+++ Main.py
@@ -1,46 +1,50 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 class Combustivel:
-    def __init__(self, tipo, preco, rendimento):
+    def __init__(self, tipo, preco, rendimento, prendimento, ppreco):
         self.tipo = tipo
         self.preco = preco
         self.rendimento = rendimento
-#
-#
-# jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-# jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-CombTeste = Combustivel('Teste',3.59,10)
+        self.prendimento = prendimento
+        self.ppreco = ppreco
+
+
+CombTeste = Combustivel('Teste', 3.59, 10, 0.70, 0.81)
 lista = [CombTeste]
 
 
-# @app.route('/')
-# def index():
-@app.route('/', methods=['POST',])
-def index():
-    tipo = request. form['tipo']
-    preco = request. form['preco']
-    rendimento = request. form['rendimento']
-    combustivel = Combustivel(tipo, preco, rendimento)
-    lista.append(combustivel)
-    return render_template('main.html', titulo='Homepage')
-# return render_template('main.html', titulo='Jogos', jogos=lista)
+@app.route('/')
+def home():
+    return render_template('main.html', titulo='Home')
 
-@app.route('/calcular')
+
+@app.route('/calcular', methods=['POST', ])
 def calcular():
-    return render_template('calcular.html', titulo='Resultado')
-
-
-# @app.route('/criar', methods=['POST',])
-# def criar():
-#     nome = request. form['nome']
-#     categoria = request. form['categoria']
-#     console = request. form['console']
-#     jogo = Jogo(nome, categoria, console)
-#     lista.append(jogo)
-#     return redirect('/')
+    tipo = 'Alcool'
+    preco = request.form['apreco']
+    rendimento = 7.6
+    prendimento = 0.63
+    ppreco = 0.93
+    combustivel = Combustivel(tipo, preco, rendimento, prendimento, ppreco)
+    lista.append(combustivel)
+    tipo = 'DtClean'
+    preco = request.form['dpreco']
+    rendimento = 10.6
+    prendimento = 0.71
+    ppreco = 0.71
+    combustivel = Combustivel(tipo, preco, rendimento, prendimento, ppreco)
+    lista.append(combustivel)
+    tipo = 'OctaPro'
+    preco = request.form['opreco']
+    rendimento = 12
+    prendimento = 0.88
+    ppreco = 1.31
+    combustivel = Combustivel(tipo, preco, rendimento, prendimento, ppreco)
+    lista.append(combustivel)
+    return render_template('calcular.html', titulo='Resultado', combustiveis=lista)
 
 
 app.run(debug=True)
